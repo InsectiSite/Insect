f26e3d4ea3da8deea188ac6db0dd1b12f932fea1
Rh4ndon <daverhandon@gmail.com>
2024-04-25 23:16:47 +0800

    updates on about us and navigation

---
+++ quizis.js
@@ -0,0 +1,109 @@
+let questions = [
+    {
+      numb: 1,
+      question: "Which insects is famous for its role in pollonating flowers?",
+      answer: "Bee",
+      options: [
+        "Butterfly",
+        "Bee",
+        "Grasshopper",
+        "Dragonfly"
+      ]
+    },
+    {
+      numb: 2,
+      question: "Which of the following is not an insect?",
+      answer: "Spider",
+      options: [
+        "Butterfly",
+        "Spider",
+        "Beetle",
+        "Ant"
+      ]
+    },
+    {
+      numb: 3,
+      question: "What do butterflies start out as before they become butterflies?",
+      answer: "Caterpillar",
+      options: [
+        "Caterpillar",
+        "Larvae",
+        "Pupae",
+        "Nymphs"
+      ]
+    },
+    {
+      numb: 4,
+      question: "What do bees collect to make honey?",
+      answer: "Nectar",
+      options: [
+        "Pollen",
+        "Nectar",
+        "Leaves",
+        "Water"
+      ]
+    },
+    {
+      numb: 5,
+      question: "What is the name of the substance that covers the wings of butterflies and moths?",
+      answer: "Scales",
+      options: [
+        "Scales",
+        "Feathers",
+        "Fur",
+        "Shells"
+      ]
+    },
+    {
+      numb: 6,
+      question: "What is the primary function of an insect's antennae?",
+      answer: "Smelling",
+      options: [
+        "Smelling",
+        "Hearing", 
+        "Tasting", 
+        "Seeing"
+      ]
+    },
+    {
+      numb: 7,
+      question: "Which insect is known for its ability to jump long distances?",
+      answer: "Grasshopper",
+      options: 
+      ["Beetle", 
+      "Ant", 
+      "Grasshopper", 
+      "Mosquito"]
+    },
+    {
+      numb: 8,
+      question: "Which insect is often associated with carrying diseases like malaria and dengue fever?",
+      answer: "Mosquito",
+      options: 
+      ["Mosquito", 
+      "Firefly", 
+      "Termite", 
+      "Dragonfly"]
+    },
+    {
+      numb: 9,
+      question: "What do ants live in?",
+      answer: "Nests",
+      options: [
+        "Nests",
+        "Hives",
+        "Colonies",
+        "Dens"
+      ]
+    },
+    {
+      numb: 10,
+      question: "Which insect undergoes a process called metamorphosis during its development?",
+      answer: "All of the above",
+      options: [
+        "Beetle", 
+        "Dragonfly", 
+        "Grasshopper", 
+        "All of the above"]
+    }
+  ];
